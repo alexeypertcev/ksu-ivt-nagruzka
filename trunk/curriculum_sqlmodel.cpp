5275a95a42265fba336b0cad5bb1d00bc19b3677
alexey.pertcev@gmail.com <alexey.pertcev@gmail.com@440bbf42-0d3f-4c19-b681-2d4df9560f4c>
2011-05-14 16:27:48 +0000

    небольшие изменения в архитектуре

--- trunk/curriculum_sqlmodel.cpp
+++ trunk/curriculum_sqlmodel.cpp
@@ -15,19 +15,18 @@ CurriculumSqlModel::CurriculumSqlModel(QObject *parent)
 Qt::ItemFlags CurriculumSqlModel::flags(
         const QModelIndex &index) const
 {
+    Qt::ItemFlags flags = QSqlQueryModel::flags(index);
     if (index.column() == 1 || index.column() == 2 ||
-        index.column() == 3 || /*index.column() == 4 ||*/
+        index.column() == 3 || index.column() == 4 ||
         index.column() == 5 || index.column() == 6 ||
         index.column() == 7)
     {
-        return Qt::ItemIsEditable;
+        flags |= Qt::ItemIsEditable;
     } else {
-        if (index.column() == 4 )
-        {
-            //return QAbstractItemModel::flags(index);
-        }
+       // flags = Qt::NoItemFlags;
     }
-    return QSqlQueryModel::flags(index);
+
+    return flags;
 }
 
 bool CurriculumSqlModel::setData(const QModelIndex &index, const QVariant &value, int /* role */)
@@ -76,45 +75,4 @@ bool CurriculumSqlModel::setLastName(int personId, const QString &lastName)
     query.addBindValue(lastName);
     query.addBindValue(personId);
     return query.exec();
-}
-
-SpinBoxDelegate::SpinBoxDelegate(QObject *parent)
-    : QItemDelegate(parent)
-{
-}
-
-QWidget *SpinBoxDelegate::createEditor(QWidget *parent,
-    const QStyleOptionViewItem &/* option */,
-    const QModelIndex &/* index */) const
-{
-    QSpinBox *editor = new QSpinBox(parent);
-    editor->setMinimum(0);
-    editor->setMaximum(100);
-
-    return editor;
-}
-
-void SpinBoxDelegate::setEditorData(QWidget *editor,
-                                    const QModelIndex &index) const
-{
-    int value = index.model()->data(index, Qt::EditRole).toInt();
-
-    QSpinBox *spinBox = static_cast<QSpinBox*>(editor);
-    spinBox->setValue(value);
-}
-
-void SpinBoxDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
-                                   const QModelIndex &index) const
-{
-    QSpinBox *spinBox = static_cast<QSpinBox*>(editor);
-    spinBox->interpretText();
-    int value = spinBox->value();
-
-    model->setData(index, value, Qt::EditRole);
-}
-
-void SpinBoxDelegate::updateEditorGeometry(QWidget *editor,
-    const QStyleOptionViewItem &option, const QModelIndex &/* index */) const
-{
-    editor->setGeometry(option.rect);
 }

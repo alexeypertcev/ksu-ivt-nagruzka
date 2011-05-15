e8d2253865c7fa82a306e4ce512c943a42529b1f
alexey.pertcev@gmail.com <alexey.pertcev@gmail.com@440bbf42-0d3f-4c19-b681-2d4df9560f4c>
2011-05-15 12:16:52 +0000

    Работа с таблицей Учебный план закончена

--- delegates.cpp
+++ delegates.cpp
@@ -1,49 +1,7 @@
 /****************************************************************************
 **
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
-**     the names of its contributors may be used to endorse or promote
-**     products derived from this software without specific prior written
-**     permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-** $QT_END_LICENSE$
 **
 ****************************************************************************/
-
-/*
-    delegate.cpp
-
-    A delegate that allows the user to change integer values from the model
-    using a spin box widget.
-*/
 
 #include <QtGui>
 #include <QtSql>
@@ -51,17 +9,13 @@
 
 #include "delegates.h"
 
-
-//! [0]
 SpinBoxDelegate::SpinBoxDelegate(int min, int max, QObject *parent)
     : QItemDelegate(parent)
 {
     range_min = min;
     range_max = max;
 }
-//! [0]
 
-//! [1]
 QWidget *SpinBoxDelegate::createEditor(QWidget *parent,
     const QStyleOptionViewItem &/* option */,
     const QModelIndex &/* index */) const
@@ -72,9 +26,7 @@ QWidget *SpinBoxDelegate::createEditor(QWidget *parent,
 
     return editor;
 }
-//! [1]
 
-//! [2]
 void SpinBoxDelegate::setEditorData(QWidget *editor,
                                     const QModelIndex &index) const
 {
@@ -83,9 +35,7 @@ void SpinBoxDelegate::setEditorData(QWidget *editor,
     QSpinBox *spinBox = static_cast<QSpinBox*>(editor);
     spinBox->setValue(value);
 }
-//! [2]
 
-//! [3]
 void SpinBoxDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
                                    const QModelIndex &index) const
 {
@@ -95,15 +45,12 @@ void SpinBoxDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
 
     model->setData(index, value, Qt::EditRole);
 }
-//! [3]
 
-//! [4]
 void SpinBoxDelegate::updateEditorGeometry(QWidget *editor,
     const QStyleOptionViewItem &option, const QModelIndex &/* index */) const
 {
     editor->setGeometry(option.rect);
 }
-//! [4]
 
 //---------------------------------------------------------------------------------------
 ComboBoxDelegate::ComboBoxDelegate(QString table_name, QObject *parent)
@@ -117,11 +64,11 @@ QWidget *ComboBoxDelegate::createEditor(QWidget *parent,
     const QModelIndex &/* index */) const
 {
     QComboBox *editor = new QComboBox(parent);
-    QSqlTableModel* tablemodel_spec = new QSqlTableModel(parent);
-    tablemodel_spec->setTable(table);
-    tablemodel_spec->setEditStrategy(QSqlTableModel::OnFieldChange);
-    tablemodel_spec->select();
-    editor->setModel(tablemodel_spec);
+    QSqlTableModel* tablemodel = new QSqlTableModel(parent);
+    tablemodel->setTable(table);
+    tablemodel->setEditStrategy(QSqlTableModel::OnFieldChange);
+    tablemodel->select();
+    editor->setModel(tablemodel);
 
     return editor;
 }
@@ -133,7 +80,7 @@ void ComboBoxDelegate::setEditorData(QWidget *editor,
 
     QComboBox *comboBox = static_cast<QComboBox*>(editor);
     int i;
-    //опасная хуйня, но быстрая)
+    //опасная хуйня, но быстрая) тк. данные из модели ошибок быть не должно
     for (i = 0; i<comboBox->count(); ++i)
     {
         if ( comboBox->itemText(i) == value ){ break; }
@@ -157,4 +104,3 @@ void ComboBoxDelegate::updateEditorGeometry(QWidget *editor,
 {
     editor->setGeometry(option.rect);
 }
-

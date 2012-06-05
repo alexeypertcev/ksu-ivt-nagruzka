9bbeedc6bf7c8557c1fa0ae018e6cd4b668a4fe1
alexey.pertcev@gmail.com <alexey.pertcev@gmail.com@440bbf42-0d3f-4c19-b681-2d4df9560f4c>
2012-06-05 19:19:25 +0000

    b

--- trunk/tab_reports.h
+++ trunk/tab_reports.h
@@ -4,21 +4,29 @@
 #include <QtXml>
 #include <QtCore/QFile>
 #include <QSqlQuery>
+#include <QMessageBox>
 #include "report_tabledates.h"
 #include "errors.h"
 
 #include "libmsdoc/cdocxlsx.h"
 
-bool create_report(QStringList teachers_id_list, QString template_patch, QString report_patch, QString type_report);
-bool create_report_ods( QList<Tabledata> list_tabledata, QString template_patch, QString report_patch);
-bool create_report_xlsx(QList<Tabledata> list_tabledata, QString template_patch, QString report_patch);
-
-class reports_creater
+class Reports_creater
 {
 public:
-    reports_creater();
+    /*
+     report_for_teachers
+     report_for_kafedry
+
+     report_for_subject
+
+     */
+
+    Reports_creater();
+    bool create_report(QStringList teachers_id_list, QString template_patch, QString report_patch, QString type_report);
+    bool create_report_ods( QList<Tabledata> list_tabledata, QString template_path, QString report_path);
+    bool create_report_xlsx(QList<Tabledata> list_tabledata, QString template_path, QString report_path);
+
+    bool create_report_for_kafedry(QString report_path);
 };
-
-
 
 #endif // REPORTS_CREATER_H

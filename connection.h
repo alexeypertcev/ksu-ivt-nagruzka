eeb47b96dfaa844f32556693cacec8e88c6a4b23
alexey.pertcev@gmail.com <alexey.pertcev@gmail.com@440bbf42-0d3f-4c19-b681-2d4df9560f4c>
2012-06-08 12:54:36 +0000

    багфиксы

--- connection.h
+++ connection.h
@@ -15,6 +15,133 @@
 
 QSqlDatabase db;
 
+const QString foreign_keys_ON = "PRAGMA foreign_keys = ON;";
+const QString create_table_form_training =
+        "CREATE TABLE form_training ( "
+        "name TEXT NOT NULL, "
+        "CONSTRAINT name PRIMARY KEY (name))";
+const QString create_table_subject =
+        "CREATE TABLE subject ( "
+        "name TEXT NOT NULL, "
+        "CONSTRAINT name PRIMARY KEY (name))";
+const QString create_table_speciality =
+        "CREATE TABLE speciality ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "faculty_name TEXT NOT NULL,"
+        "special_name TEXT NOT NULL, "
+        "form_training_name TEXT NOT NULL,"
+        "CONSTRAINT form_training_name FOREIGN KEY (form_training_name) "
+        "REFERENCES form_training (name))";
+const QString create_table_students =
+        "CREATE TABLE students ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "speciality_id INTEGER NOT NULL, "
+        "course INTEGER NOT NULL, "
+        "num_group INTEGER NOT NULL, "
+        "num_undergroup INTEGER NOT NULL, "
+        "quantity_course INTEGER NOT NULL, "
+        "CONSTRAINT speciality_id FOREIGN KEY (speciality_id) "
+        "REFERENCES speciality (id))";
+const QString create_table_status =
+        "CREATE TABLE status ( "
+        "name TEXT NOT NULL, "
+        "hours INTEGER NOT NULL, "
+        "CONSTRAINT name PRIMARY KEY (name))";
+const QString create_table_staff =
+        "CREATE TABLE staff ( "
+        "id INTEGER NOT NULL, "
+        "name TEXT NOT NULL, "
+        "CONSTRAINT id PRIMARY KEY (id))";
+const QString create_table_teachers =
+        "CREATE TABLE teachers ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "f TEXT NOT NULL, "
+        "i TEXT NOT NULL, "
+        "o TEXT NOT NULL, "
+        "status_name TEXT, "
+        "rate REAL NOT NULL, "
+        "staff_id INTEGER NOT NULL, "
+        "CONSTRAINT staff_id FOREIGN KEY (staff_id) "
+        " REFERENCES staff (id), "
+        "CONSTRAINT status_name FOREIGN KEY (status_name) "
+        " REFERENCES status (name)) ";
+const QString create_table_curriculum =
+        "CREATE TABLE curriculum ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "speciality_id INTEGER NOT NULL, "
+        "subject_name TEXT NOT NULL, "
+        "semmester INTEGER NOT NULL, "
+        "lection_hr INTEGER NOT NULL, "
+        "labs_hr INTEGER NOT NULL, "
+        "practice_hr INTEGER NOT NULL, "
+        "controlwork INTEGER NOT NULL, "
+        "KCP_hr INTEGER NOT NULL, "
+        "is_examen INTEGER NOT NULL, "
+        "is_offset INTEGER NOT NULL, "
+        "is_coursework INTEGER NOT NULL, "
+        "CONSTRAINT subject_name FOREIGN KEY (subject_name) "
+        " REFERENCES subject (name), "
+        "CONSTRAINT speciality_id FOREIGN KEY (speciality_id) "
+        " REFERENCES speciality (id))";
+const QString create_table_subjects_in_semmester =
+        "CREATE TABLE subjects_in_semmester ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "curriculum_id INTEGER NOT NULL, "
+        "students_id INTEGER NOT NULL, "
+        "lection_hr INTEGER NOT NULL, "
+        "labs_hr INTEGER NOT NULL, "
+        "practice_hr INTEGER NOT NULL, "
+        "individ_hr REAL NOT NULL, "
+        "kontr_rab_hr REAL NOT NULL, "
+        "consultation_hr REAL NOT NULL, "
+        "offset_hr REAL NOT NULL, "
+        "examen_hr REAL NOT NULL, "
+        "coursework_hr REAL NOT NULL, "
+        "diplomwork_hr REAL NOT NULL, "
+        "praktika_hr REAL NOT NULL, "
+        "gak_hr REAL NOT NULL, "
+        "other1 REAL NOT NULL, "
+        "other2 REAL NOT NULL, "
+        "other3 REAL NOT NULL, "
+        "CONSTRAINT students_id FOREIGN KEY (students_id) "
+        " REFERENCES students (id), "
+        "CONSTRAINT curriculum_id FOREIGN KEY (curriculum_id) "
+        " REFERENCES curriculum (id)) ";
+const QString create_table_distribution =
+        "CREATE TABLE distribution ( "
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "teachers_id INTEGER NOT NULL, "
+        "subjects_in_semmester_id INTEGER NOT NULL, "
+        "lection_hr INTEGER NOT NULL, "
+        "labs_hr INTEGER NOT NULL, "
+        "practice_hr INTEGER NOT NULL, "
+        "individ_hr REAL NOT NULL, "
+        "kontr_rab_hr REAL NOT NULL, "
+        "consultation_hr REAL NOT NULL, "
+        "offset_hr REAL NOT NULL, "
+        "examen_hr REAL NOT NULL, "
+        "coursework_hr REAL NOT NULL, "
+        "diplomwork_hr REAL NOT NULL, "
+        "praktika_hr REAL NOT NULL, "
+        "gak_hr REAL NOT NULL, "
+        "other1 REAL NOT NULL, "
+        "other2 REAL NOT NULL, "
+        "other3 REAL NOT NULL, "
+        "CONSTRAINT subjects_in_semmester_id FOREIGN KEY (subjects_in_semmester_id) "
+        " REFERENCES subjects_in_semmester (id), "
+        "CONSTRAINT teachers_id FOREIGN KEY (teachers_id) "
+        " REFERENCES teachers (id))";
+const QString create_table_coefficients =
+        "CREATE TABLE coefficients ( "
+        "name TEXT NOT NULL, "
+        "value REAL NOT NULL, "
+        "CONSTRAINT name PRIMARY KEY (name))";
+const QString create_table_other_data =
+        "CREATE TABLE other_data ( "
+        "name TEXT NOT NULL, "
+        "value TEXT NOT NULL, "
+        "CONSTRAINT name PRIMARY KEY (name))";
+
 static bool createConnection(QString path_db)
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
@@ -44,120 +171,19 @@ static bool create_all_tables(){
         return false;
     } else{
         QSqlQuery query;
-        query.exec("PRAGMA foreign_keys = ON;");
-        query.exec("CREATE TABLE form_training ( "
-                   "name TEXT NOT NULL, "
-                   "CONSTRAINT name PRIMARY KEY (name))");
-        query.exec("CREATE TABLE subject ( "
-                   "name TEXT NOT NULL, "
-                   "CONSTRAINT name PRIMARY KEY (name))");
-        query.exec("CREATE TABLE speciality ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "faculty_name TEXT NOT NULL,"
-                   "special_name TEXT NOT NULL, "
-                   "form_training_name TEXT NOT NULL,"
-                   "CONSTRAINT form_training_name FOREIGN KEY (form_training_name) "
-                   "REFERENCES form_training (name))");
-        query.exec("CREATE TABLE students ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "speciality_id INTEGER NOT NULL, "
-                   "course INTEGER NOT NULL, "
-                   "num_group INTEGER NOT NULL, "
-                   "num_undergroup INTEGER NOT NULL, "
-                   "quantity_course INTEGER NOT NULL, "
-                   "CONSTRAINT speciality_id FOREIGN KEY (speciality_id) "
-                   "REFERENCES speciality (id))");
-        query.exec("CREATE TABLE status ( "
-                   "name TEXT NOT NULL, "
-                   "hours INTEGER NOT NULL, "
-                   "CONSTRAINT name PRIMARY KEY (name))");
-        query.exec("CREATE TABLE staff ( "
-                   "id INTEGER NOT NULL, "
-                   "name TEXT NOT NULL, "
-                   "CONSTRAINT id PRIMARY KEY (id))");
-        query.exec("CREATE TABLE teachers ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "f TEXT NOT NULL, "
-                   "i TEXT NOT NULL, "
-                   "o TEXT NOT NULL, "
-                   "status_name TEXT, "
-                   "rate REAL NOT NULL, "
-                   "staff_id INTEGER NOT NULL, "
-                   "CONSTRAINT staff_id FOREIGN KEY (staff_id) "
-                   "  REFERENCES staff (id), "
-                   "CONSTRAINT status_name FOREIGN KEY (status_name) "
-                   "  REFERENCES status (name)) ");
-        query.exec("CREATE TABLE curriculum ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "speciality_id INTEGER NOT NULL, "
-                   "subject_name TEXT NOT NULL, "
-                   "semmester INTEGER NOT NULL, "
-                   "lection_hr INTEGER NOT NULL, "
-                   "labs_hr INTEGER NOT NULL, "
-                   "practice_hr INTEGER NOT NULL, "
-                   "controlwork INTEGER NOT NULL, "
-                   "KCP_hr INTEGER NOT NULL, "
-                   "is_examen INTEGER NOT NULL, "
-                   "is_offset INTEGER NOT NULL, "
-                   "is_coursework INTEGER NOT NULL, "
-                   "CONSTRAINT subject_name FOREIGN KEY (subject_name) "
-                   "  REFERENCES subject (name), "
-                   "CONSTRAINT speciality_id FOREIGN KEY (speciality_id) "
-                   "  REFERENCES speciality (id))");
-        query.exec("CREATE TABLE subjects_in_semmester ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "curriculum_id INTEGER NOT NULL, "
-                   "students_id INTEGER NOT NULL, "
-                   "lection_hr INTEGER NOT NULL, "
-                   "labs_hr INTEGER NOT NULL, "
-                   "practice_hr INTEGER NOT NULL, "
-                   "individ_hr REAL NOT NULL, "
-                   "kontr_rab_hr REAL NOT NULL, "
-                   "consultation_hr REAL NOT NULL, "
-                   "offset_hr REAL NOT NULL, "
-                   "examen_hr REAL NOT NULL, "
-                   "coursework_hr REAL NOT NULL, "
-                   "diplomwork_hr REAL NOT NULL, "
-                   "praktika_hr REAL NOT NULL, "
-                   "gak_hr REAL NOT NULL, "
-                   "other1 REAL NOT NULL, "
-                   "other2 REAL NOT NULL, "
-                   "other3 REAL NOT NULL, "
-                   "CONSTRAINT students_id FOREIGN KEY (students_id) "
-                   "  REFERENCES students (id), "
-                   "CONSTRAINT curriculum_id FOREIGN KEY (curriculum_id) "
-                   "  REFERENCES curriculum (id)) ");
-        query.exec("CREATE TABLE distribution ( "
-                   "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                   "teachers_id INTEGER NOT NULL, "
-                   "subjects_in_semmester_id INTEGER NOT NULL, "
-                   "lection_hr INTEGER NOT NULL, "
-                   "labs_hr INTEGER NOT NULL, "
-                   "practice_hr INTEGER NOT NULL, "
-                   "individ_hr REAL NOT NULL, "
-                   "kontr_rab_hr REAL NOT NULL, "
-                   "consultation_hr REAL NOT NULL, "
-                   "offset_hr REAL NOT NULL, "
-                   "examen_hr REAL NOT NULL, "
-                   "coursework_hr REAL NOT NULL, "
-                   "diplomwork_hr REAL NOT NULL, "
-                   "praktika_hr REAL NOT NULL, "
-                   "gak_hr REAL NOT NULL, "
-                   "other1 REAL NOT NULL, "
-                   "other2 REAL NOT NULL, "
-                   "other3 REAL NOT NULL, "
-                   "CONSTRAINT subjects_in_semmester_id FOREIGN KEY (subjects_in_semmester_id) "
-                   "  REFERENCES subjects_in_semmester (id), "
-                   "CONSTRAINT teachers_id FOREIGN KEY (teachers_id) "
-                   "  REFERENCES teachers (id))");
-        query.exec("CREATE TABLE coefficients ( "
-                   "name TEXT NOT NULL, "
-                   "value REAL NOT NULL, "
-                   "CONSTRAINT name PRIMARY KEY (name))");
-        query.exec("CREATE TABLE other_data ( "
-                   "name TEXT NOT NULL, "
-                   "value TEXT NOT NULL, "
-                   "CONSTRAINT name PRIMARY KEY (name))");
+        query.exec(foreign_keys_ON);
+        query.exec(create_table_form_training);
+        query.exec(create_table_subject);
+        query.exec(create_table_speciality);
+        query.exec(create_table_students);
+        query.exec(create_table_status);
+        query.exec(create_table_staff);
+        query.exec(create_table_teachers);
+        query.exec(create_table_curriculum);
+        query.exec(create_table_subjects_in_semmester);
+        query.exec(create_table_distribution);
+        query.exec(create_table_coefficients);
+        query.exec(create_table_other_data);
     }
     return true;
 }
@@ -177,14 +203,13 @@ static bool insert_main_data()
         QSqlQuery query;
 
         // main data
-        query.exec("PRAGMA foreign_keys = ON;");
+        query.exec(foreign_keys_ON);
+
         query.exec("insert into form_training values('оч')");
         query.exec("insert into form_training values('оч-заоч')");
         query.exec("insert into form_training values('заоч')");
         query.exec("insert into speciality values(NULL, 'ФИВТ','МОиАИС', 'оч')");
-        query.exec("insert into speciality values(NULL, 'ФИВТ','МОиАИС', 'оч-заоч')");
-        query.exec("insert into speciality values(NULL, 'ФИВТ','ПО', 'оч')");
-        query.exec("insert into speciality values(NULL, 'ФИВТ','ИАЯ', 'оч')");
+
         query.exec("insert into status values('выберите..', 0)");
         query.exec("insert into status values('профессор', 680)");
         query.exec("insert into status values('доцент', 730)");
@@ -192,9 +217,11 @@ static bool insert_main_data()
         query.exec("insert into status values('ассистент', 830)");
         query.exec("insert into status values('декан', 500)");
         query.exec("insert into status values('зав. кафедрой', 700)");
+
         query.exec("insert into staff values(0, 'штатный')");
         query.exec("insert into staff values(1, 'внешний совместитель')");
         query.exec("insert into staff values(2, 'внутр.')");
+
         query.exec("insert into teachers values(0, 'выберите..', ' ', ' ', 'выберите..', 1 , 0);");
 
         query.exec("insert into coefficients values('coefficient_lection_hr', 1)");
@@ -209,8 +236,6 @@ static bool insert_main_data()
         query.exec("insert into coefficients values('coefficient_consultation_zaochnui_percent', 15)");
         query.exec("insert into coefficients values('coefficient_consultation_och_zaoch_percent', 10)");
         query.exec("insert into coefficients values('coefficient_consultation_add_is_examen_for_group', 2)");
-
-        // not found
         query.exec("insert into coefficients values('coefficient_ruk_vo_kurs_work_hr', 5)");
         query.exec("insert into coefficients values('coefficient_ruk_vo_VKR_spec_hr', 20)");
         query.exec("insert into coefficients values('coefficient_ruk_vo_VKR_bak_hr', 12)");
@@ -220,48 +245,12 @@ static bool insert_main_data()
         query.exec("insert into coefficients values('coefficient_normokontrol_hr', 1)");
         query.exec("insert into coefficients values('coefficient_ychastie_work_GAK_min', 30)");
         query.exec("insert into coefficients values('coefficient_ruk_vo_aspirants_hr', 50)");
-        //
-
         query.exec("insert into other_data values('academic_year', '2011/2012')");
         query.exec("insert into other_data values('name_kafedry_faculty', 'Программного обеспечения и администрирования информационных систем')");
         query.exec("insert into other_data values('name_kafedry_smail', 'ПОиАИС')");
         query.exec("insert into other_data values('business_base_of_training', 'бюджет')");
         query.exec("insert into other_data values('vice_rector_on_education_work', 'Захаров В.В.')");
 
-        //from anatoly zhmakin
-
-        query.exec("insert into subject values('Информационные системы')");
-        query.exec("insert into subject values('Основы микроэлектроники')");
-        query.exec("insert into subject values('Архитектура компьютера')");
-        query.exec("insert into subject values('Информационные системы')");
-        query.exec("insert into subject values('Компьютерные сети, интернет и мультимедиа технологии')");
-        query.exec("insert into subject values('Компьютерное моделирование')");
-
-        query.exec("insert into teachers values(NULL, 'Жмакин', 'Анатолий', 'Петрович', 'зав. кафедрой', 1.2 , 0 );");
-        query.exec("insert into teachers values(NULL, 'Лопин', 'Вячеслав', 'Николаевич', 'профессор', 1.0 , 0 );");
-        query.exec("insert into teachers values(NULL, 'Бабкин', 'Евгений', 'Александрович', 'профессор', 1.0 , 0 );");
-        query.exec("insert into teachers values(NULL, 'Кудинов', 'Виталий', 'Алексеевич', 'профессор', 0.5 , 0 );");
-
-        query.exec("insert into students values(NULL, 1, 1, 1, 2, 25 );");
-        query.exec("insert into students values(NULL, 1, 2, 1, 2, 22 );");
-        query.exec("insert into students values(NULL, 1, 3, 2, 3, 32 );");
-        query.exec("insert into students values(NULL, 1, 4, 2, 3, 28 );");
-        query.exec("insert into students values(NULL, 1, 5, 1, 2, 28 );");
-        query.exec("insert into students values(NULL, 2, 5, 1, 1, 6 );");
-        query.exec("insert into students values(NULL, 2, 6, 1, 1, 8 );");
-        query.exec("insert into students values(NULL, 4, 3, 1, 2, 15 );");
-        query.exec("insert into students values(NULL, 4, 4, 1, 2, 19 );");
-        query.exec("insert into students values(NULL, 4, 5, 1, 2, 21 );");
-        query.exec("insert into students values(NULL, 3, 2, 1, 2, 16 );");
-        query.exec("insert into students values(NULL, 3, 3, 1, 1, 13 );");
-        query.exec("insert into students values(NULL, 3, 4, 1, 2, 16 );");
-
-        query.exec("insert into curriculum values(NULL, 1, 'Информатика',1, 36, 34, 0, 0, 1, 0, 0 );");
-        query.exec("insert into curriculum values(NULL, 1, 'Программирование',1, 36, 30, 0, 2, 0, 0, 1, 0 );");
-        query.exec("insert into curriculum values(NULL, 1, 'Программирование',2, 32, 30, 0, 0, 0, 1, 0, 0 );");
-        query.exec("insert into curriculum values(NULL, 1, 'Введение в системное программирование',2, 20, 18, 0, 0, 0, 0, 1, 0 );");
-        query.exec("insert into curriculum values(NULL, 1, 'Программирование',3, 0, 0, 16, 0, 0, 0, 1, 1 );");
-        query.exec("insert into curriculum values(NULL, 1, 'Структуры и алгоритмы компьютерной обработки данных',3, 36, 36, 0, 0, 0, 0, 1, 0 );");
      }
     return true;
 }
